5548acf2e9f11df1bbfbeeea9fd4863d24634e87
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-25 15:18:36 +0300

    added an urls for the insta.

--- gram/urls.py
+++ gram/urls.py
@@ -2,3 +2,9 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns =[
+    url('^$', views.landing_page, name = 'landing_page')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+import datetime as dt
+from .models import Image,Profile,Comment
+from email import send_welcome_email
 
 # Create your views here.
+@login_required
+def landing_page(request):
+    return render(request,'index.html')
+def search_results(request):
+    if 'image' in request.GET and request.GET['image']:
+        search_input =request.GET.get('image')
+        searched_images =Image.search_by_category(search_input)
+        message = f"{search_input}"
+        return render(request,'search.html',{"message":message,"images":searched_images})
+    else:
+        message = "Please input something in the search field"
+        return render(request,"search.html",{"message":message})
+def single_pic(request):
+    image=Image.get_pic()
+    return render(request, "singlepic.html",{"image":image})
+
