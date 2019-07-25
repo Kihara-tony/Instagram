361fa460bfb16a962aedb38a909508d75f425020
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-25 14:40:51 +0300

    added tests for image and profile.

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Comment,Image,Profile
+import datetime as dt
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+
+        # Creating a new tag and saving it
+        self.new_image = Image(name = 'testing')
+        self.new_image.save()
+
+        self.new_profile= Profile(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_profile.save()
+
+        self.new_image.profile.add(self.new_profile)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
