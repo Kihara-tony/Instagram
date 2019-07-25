282c170f4cc2f4f2cc879d58750868b30f078c71
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-25 23:00:03 +0300

    added imports of url,views,settings and static. Also added urlpatterns for profile editing,account,search and single picture in the urls file of app gram.

--- gram/urls.py
+++ gram/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
-from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns =[
-    url('^$', views.landing_page, name = 'landing_page')
+urlpatterns=[
+    url(r'im/(\d+)',views.image,name = 'singleImage'),
+    url(r'^$',views.account,name = 'account'),
+    url(r'^search/',views.search,name = 'search'),
+    url(r'^signout/$', views.signout, name='signout'),
+    url(r'^account/(?P<username>[-_\w.]+)/edit/$',views.profile_edit,name='profile_edit'),
+    url(r'^post/$',views.post_picture,name='post'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
