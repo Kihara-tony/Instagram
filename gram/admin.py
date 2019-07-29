79ef80ecd4b4bc7fd9b69121fc1c91b64c07b9d4
Tony Kihara <tonykiharatonkin@gmail.com>
2019-07-30 02:30:38 +0300

    added registers in the admin dashboard.

--- gram/admin.py
+++ gram/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Profile,Image,Comment
-
 # Register your models here.
-class ProfileAdmin(admin.ModelAdmin):
-    filter_horizontal = ('Profile',)
+from .models import Location,tags, Image
 
-admin.site.register(Profile)
-admin.site.register(Image)
-admin.site.register(Comment)
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+admin.site.register(Location)
+admin.site.register(tags)
+admin.site.register(Image, ImageAdmin)
